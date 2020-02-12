00be54aa92bfc9c00e33e30a22582854cb5ad2fc
Leandro <leandrobruceadc@gmail.com>
2020-02-11 22:00:27 -0300

    First Commit. Added a CRUD example

---
+++ projeto_crud/navBar.php
@@ -0,0 +1,25 @@
+<!-- INICIO DO NAVBAR -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navBar">
+    <!-- 1.0 -->
+    <a class="navbar-brand" href="#">
+      <!-- 1.1 -->
+      <img src="img/logo1.png" width="90" height="90" class="d-inline-block align-center" alt="">
+      <b>King Investment News</b>
+    </a> <!-- 1.1 -->
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <!-- 1.2 -->
+      <div class="navbar-nav">
+        <!-- 1.3 -->
+        <a class="nav-item nav-link" href="index.php"><b>Home</b></a>
+        <a class="nav-item nav-link" href="conversor.php"><b>Conversor</b></a>
+        <a class="nav-item nav-link" href="simulador.php"><b>Simulador</b></a>
+      </div> <!-- 1.3 -->
+    </div> <!-- 1.2 -->
+    <form class="form-inline">
+      <!-- 1.4 -->
+      <a class="nav-item nav-link" href="editar.php" style="color: black">Trocar senha</a>
+      <button class="btn btn-sm btn-outline-secondary" type="button"><a href="logout.php" style="color: black; text-decoration:none;">SAIR</a></button>
+
+    </form> <!-- 1.4 -->
+  </nav> <!-- 1.0 -->
+ <!-- FIM DO NAV BAR -->
